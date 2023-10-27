0b049f250c9cc391a4b3d6601cb0cab7091f08c1
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-10-27 20:04:14 +0300

    init

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,53 +1,23 @@
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using NUnit.Framework;
 
 namespace PlaywrightTests;
 
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
-public class Tests : PageTest
+public class MyTest : PageTest
 {
     [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    public async Task ShouldHaveTheCorrectSlogan()
     {
         await Page.GotoAsync("https://playwright.dev");
+        await Expect(Page.Locator("text=enables reliable end-to-end testing for modern web apps")).ToBeVisibleAsync();
+    }
 
-        // Expect a title "to contain" a substring.
-        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
-
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
-
-        // Click the get started link.
-        await getStarted.ClickAsync();
-
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
-
-        await using var browser = await Playwright.Chromium.LaunchAsync();
-await using var context = await browser.NewContextAsync();
-
-// Start tracing before creating / navigating a page.
-await context.Tracing.StartAsync(new()
-{
-    Screenshots = true,
-    Snapshots = true,
-    Sources = true
-});
-
-var page = await context.NewPageAsync();
-await page.GotoAsync("https://playwright.dev");
-
-// Stop tracing and export it into a zip archive.
-await context.Tracing.StopAsync(new()
-{
-    Path = "trace.zip"
-});
+    [Test]
+    public async Task ShouldHaveTheCorrectTitle()
+    {
+        await Page.GotoAsync("https://playwright.dev");
+        var title = Page.Locator(".navbar__inner .navbar__title");
+        await Expect(title).ToHaveTextAsync("Playwright");
     }
 }
